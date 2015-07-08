a2a250a79951a24a4c6bc488a9209c7ab172426e
Daniel Chen <danio.chen@gmail.com>
2015-07-08 13:23:23 -0700

    minor ui improvement

--- contmon/content/admin.py
+++ contmon/content/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from .models import CrawlUrl, CrawledPage, CreditCardOffer
 from reversion_compare.admin import CompareVersionAdmin
 
-@admin.register(CrawlUrl, CrawledPage)
-class CrawlContentAdmin(CompareVersionAdmin):
+@admin.register(CrawlUrl)
+class CrawlUrlAdmin(CompareVersionAdmin):
+    list_display = ('url', 'domain',)
+    list_filter = ('domain',)
+
+@admin.register(CrawledPage)
+class CrawledPageAdmin(CompareVersionAdmin):
     pass
 
 @admin.register(CreditCardOffer)
 class CreditCardOfferADmin(CompareVersionAdmin):
     readonly_fields = ('extracted_fields',)
-    list_display = ('domain',)
+    list_display = ('domain','name', 'created')
     list_filter = ('domain',)
